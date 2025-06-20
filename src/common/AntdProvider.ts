96f7743d11f08063792eed0d15afcdde70bedd56
julian <julxxy@outlook.com>
2025-06-20 22:34:00 +0800

    feat: Add license header

--- src/common/AntdProvider.ts
+++ src/common/AntdProvider.ts
@@ -1,4 +1,23 @@
 /**
+ * This file is part of xxl-job-panel-r3.
+ *
+ * Copyright (C) 2025 Julian
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * 在 App.tsx 中导入了 AntdProvider.ts 文件，并在 ConfigProvider 组件中使用了自定义主题.
  * 入口处初始化一次
  * @file Antd global message, notification and modal
--- src/utils/formValidators.ts
+++ src/utils/formValidators.ts
@@ -1,3 +1,22 @@
+/**
+ * This file is part of xxl-job-panel-r3.
+ *
+ * Copyright (C) 2025 Julian
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // 校验 Cron 表达式（Quartz 7位，简单实现）
 export function validateCronRule(_: any, value: string) {
   if (!value) return Promise.reject('请输入 Cron 表达式')
